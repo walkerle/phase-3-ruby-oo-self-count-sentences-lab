02f05cfc2e28c752990e7792119530043a5cf4c5
Walker Le <walker.le88@gmail.com>
2023-01-31 11:45:39 -0700

    commit tree 4ede3bd6aa2f575e819fb10e8eb77dd8b11b585a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with? "."
   end
 
-  def question?
+  # puts "Yes.".sentence?
 
+  def question?
+    self.end_with? "?"
   end
 
   def exclamation?
-
+    self.end_with? "!"
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).filter { |sentence| !sentence.empty? }.count
   end
+
+  puts "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
+
+  # puts "This, well, is a sentence. This is too!! And so is this, I think? Woo...".split(/\.|\?|\!/)
+
+  # puts "This, well, is a sentence. This is too!! And so is this, I think? Woo...".split(/\.|\?|\!/).filter { |sentence| !sentence.empty? }
+
+  # puts "This, well, is a sentence. This is too!! And so is this, I think? Woo...".split(/\.|\?|\!/).filter { |sentence| !sentence.empty? }.size
+
 end
